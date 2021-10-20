5139a6aaa51e4c2580a181dbbf4d18af570b3a0b
aldeny <denial753@gmail.com>
2021-10-20 17:12:41 +0700

    Perbaiki card index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,7 +7,7 @@
   <div class="container-fluid">
     <div class="row mb-4">
       <div class="col-sm-6">
-        <h1 class="m-0">Ringkasan Bisnis</h1>
+        <h1 class="m-0">Ringkasan Neraca</h1>
       </div>
       <!-- /.col -->
     </div>
@@ -16,23 +16,23 @@
     {{-- Card Ringkasan --}}
     <div class="row">
         <div class="col-md-3 col-sm-6 col-12">
-        <div class="info-box">
-            <span class="info-box-icon bg-info"><i class="fas fa-wallet"></i></span>
-            <div class="info-box-content">
-            <span class="info-box-text">Total Saldo</span>
-            <span class="info-box-number">{{ $count }}</span>
+            <div class="info-box bg-gradient-dark">
+                <span class="info-box-icon bg-success"><i class="fas fa-wallet"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Saldo</span>
+                    <span class="info-box-number mb-3">{{ $count }}</span>
+                    <a href="{{ route('credit.index') }}" class="btn btn-sm btn-info small-box-footer text-white">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
             </div>
-            <!-- /.info-box-content -->
-        </div>
-        <!-- /.info-box -->
         </div>
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-12">
-        <div class="info-box">
+        <div class="info-box bg-gradient-dark">
             <span class="info-box-icon bg-success"><i class="fas fa-money-check-alt"></i></span>
             <div class="info-box-content">
             <span class="info-box-text">Saldo Kas Bank</span>
-            <span class="info-box-number">{{ $count_bank }}</span>
+            <span class="info-box-number mb-3">{{ $count_bank }}</span>
+            <a href="{{ route('credit.index') }}" class="btn btn-sm btn-info small-box-footer text-white">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
             </div>
             <!-- /.info-box-content -->
         </div>
@@ -40,12 +40,12 @@
         </div>
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-12">
-        <div class="info-box">
+        <div class="info-box bg-gradient-dark">
             <span class="info-box-icon bg-success"><i class="fas fa-money-bill"></i></span>
-
             <div class="info-box-content">
             <span class="info-box-text">Saldo Kas Besar</span>
-            <span class="info-box-number">{{ $count_besar }}</span>
+            <span class="info-box-number mb-3">{{ $count_besar }}</span>
+            <a href="{{ route('credit.index') }}" class="btn btn-sm btn-info small-box-footer text-white">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
             </div>
             <!-- /.info-box-content -->
         </div>
@@ -53,12 +53,12 @@
         </div>
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-12">
-        <div class="info-box">
+        <div class="info-box bg-gradient-dark">
             <span class="info-box-icon bg-success"><i class="fas fa-money-bill-alt"></i></span>
-
             <div class="info-box-content">
             <span class="info-box-text">Saldo Kas Kecil</span>
-            <span class="info-box-number">{{ $count_kecil }}</span>
+            <span class="info-box-number mb-3">{{ $count_kecil }}</span>
+            <a href="{{ route('credit.index') }}" class="btn btn-sm btn-info small-box-footer text-white">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
             </div>
             <!-- /.info-box-content -->
         </div>
@@ -66,46 +66,54 @@
         </div>
         <!-- /.col -->
     </div>
+
+    <div class="row mb-4">
+        <div class="col-sm-6">
+          <h1 class="m-0">Ringkasan Alur Kas</h1>
+        </div>
+        <!-- /.col -->
+    </div>
+
     <div class="row">
-        <div class="col-md-4 col-sm-4 col-12">
-        <div class="info-box">
+        <div class="col-md-3 col-sm-3 col-12">
+        <div class="info-box bg-gradient-dark">
             <span class="info-box-icon bg-danger"><i class="fas fa-chart-line"></i></span>
             <div class="info-box-content">
-            <span class="info-box-text text-danger">Pembelian</span>
-            <span class="info-box-number text-danger">{{ $saldo_buy_ }}</span>
+                <span class="info-box-text text-white">Pembelian</span>
+                <span class="info-box-number text-white mb-3">{{ $saldo_buy_ }}</span>
+                <a href="{{ route('credit.index') }}" class="btn btn-sm btn-info small-box-footer text-white">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
             </div>
             <!-- /.info-box-content -->
         </div>
         <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-md-4 col-sm-4 col-12">
-        <div class="info-box">
+        <div class="col-md-3 col-sm-3 col-12">
+        <div class="info-box bg-gradient-dark">
             <span class="info-box-icon bg-success"><i class="fas fa-money-check-alt"></i></span>
             <div class="info-box-content">
             <span class="info-box-text">Penjualan</span>
-            <span class="info-box-number">{{ $saldo_sell_ }}</span>
+            <span class="info-box-number mb-3">{{ $saldo_sell_ }}</span>
+            <a href="{{ route('credit.index') }}" class="btn btn-sm btn-info small-box-footer text-white">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
             </div>
             <!-- /.info-box-content -->
         </div>
         <!-- /.col -->
         </div>
-        <div class="col-md-4 col-sm-4 col-12">
-            <div class="info-box">
+        <div class="col-md-3 col-sm-3 col-12">
+            <div class="info-box bg-gradient-dark">
                 <span class="info-box-icon bg-success"><i class="fas fa-money-check-alt"></i></span>
                 <div class="info-box-content">
                 <span class="info-box-text">Pendapatan</span>
-                <span class="info-box-number">{{ $cuan }}</span>
+                <span class="info-box-number mb-3">{{ $cuan }}</span>
+                <a href="{{ route('credit.index') }}" class="btn btn-sm btn-info small-box-footer text-white">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
                 <!-- /.info-box-content -->
             </div>
         <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-    </div>
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="info-box bg-gradient-gray-dark">
+        <div class="col-md-3 col-sm-3 col-12">
+            <div class="info-box bg-gradient-dark">
                 <span class="info-box-icon bg-warning"><i class="fas fa-chart-line"></i></span>
                 <div class="info-box-content">
                     <span class="info-box-text text-white">Credit/Piutang</span>
@@ -115,6 +123,7 @@
                 </div>
             </div>
         </div>
+        <!-- /.col -->
     </div>
     {{-- End Card Ringkasan --}}
     </div>
